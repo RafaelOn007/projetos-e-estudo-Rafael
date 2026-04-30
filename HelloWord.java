9df58d0fc5a03a6abb9ac5148bb079c3b43a7c49
RafaelOn007 <rafaelffmax1@gmail.com>
2026-04-30 15:33:44 -0300

    Update HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,5 @@
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("Ambiente configurado com sucesso!");
+        System.out.println("Hello, World!");
     }
 }
